7996ad2717ab178a4a5ba11ad9d25ea9979abd1a
Jasmina Jovanovic <jasminajancic@gmail.com>
2019-05-21 14:00:49 -0500

    fixing circle sizes

--- js/logic.js
+++ js/logic.js
@@ -19,7 +19,7 @@ Url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojso
 
 // create radius function
 function getRadius(magnitude) {
-    return magnitude * 10000;
+    return magnitude*25000;
 };
 
 function getColor(magnitude) {
@@ -94,10 +94,25 @@ function createMap(earthquakes) {
             accessToken: API_KEY
     });
 
+    const satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+            maxZoom: 18,
+            id: "mapbox.satellite",
+            accessToken: API_KEY
+    });
+
+    const outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+            maxZoom: 18,
+            id: "mapbox.outdoors",
+            accessToken: API_KEY
+    });
 
     // Define a baseMaps object to hold our base layers
     const baseMaps = {
             "Lighth Map": lightmap,
+            "Satellite": satellite,
+            "Outdoors": outdoors
     };
 
     // Create overlay object to hold our overlay layer
@@ -116,9 +131,10 @@ function createMap(earthquakes) {
     // Create a layer control
     // Pass in our baseMaps and overlayMaps
     // Add the layer control to the map
-    // L.control.layers(baseMaps, overlayMaps, {
-    //         collapsed: false
-    // }).addTo(myMap);
+    L.control.layers(baseMaps, overlayMaps, {
+            collapsed: false
+    }).addTo(myMap);
+
     return myMap
 }
 
